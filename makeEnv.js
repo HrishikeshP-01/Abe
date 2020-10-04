61c7c74430d60a40ec9800f1023e11ea68a71404
HrishikeshP-01 <hrishikeshpramodnair@gmail.com>
2020-10-04 15:20:40 +0530

    Made env file creation functionality

---
+++ makeEnv.js
@@ -0,0 +1,13 @@
+let $ = require('jquery')
+let fs = require('fs')
+let filename='.env'
+
+$('#make-env').on('click',()=>{
+    let DT=$('#DT').val();
+    let DG=$('#DG').val();
+    console.log(DT);
+    console.log(DG);
+    entry="DISCORD_TOKEN = "+DT+"\nDISCORD_GUILD = "+DG+"\n";
+
+    fs.appendFileSync('.env',entry)
+})
